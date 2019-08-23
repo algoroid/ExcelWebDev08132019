80d3ec0d166afdb5212ea4246ce59e4bb3be3713
Ewl <49193020+ewlmalonzo@users.noreply.github.com>
2019-08-23 01:31:05 -0700

    Add files via upload

--- Database/config/config.php
+++ Database/config/config.php
@@ -1,7 +1,7 @@
 <?php
-	define('ROOT_URL', 'http://localhost/WebDev-Malonzo/Database/');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
+	define('ROOT_URL', 'http://localhost/WebDev-Malonzo/Database/');	
+	define('DB_HOST', 'localhost');	
+	define('DB_USER', 'root');	
 	define('DB_PASS', '');
 	define('DB_NAME', 'user_account');
 ?>
