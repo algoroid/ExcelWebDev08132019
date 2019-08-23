1125225fa90ec13fea731dfffc87d48181797dc1
Ewl <49193020+ewlmalonzo@users.noreply.github.com>
2019-08-23 01:30:39 -0700

    Add files via upload

--- Database/inc/footer.php
+++ Database/inc/footer.php
@@ -1,7 +1,12 @@
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-	<script
-  src="http://code.jquery.com/jquery-1.12.4.js"
+<nav class="navbar navbar-expand-lg navbar-light bg-gradient-transparent fixed-bottom">
+	<div class="container text-center">
+		<p>Ewl Malonzo</p>
+	</div>
+</nav>
+<script
+  src="https://code.jquery.com/jquery-1.12.4.js"
   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
   crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
